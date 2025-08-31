c9184795af37486e31407acb07cbd419f5cdfba9
Flavio Iannone <w.iannone.flavio@gmail.com>
2025-08-31 20:22:45 +0200

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,56 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = "https://www.fantaprof.com";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-    return [
-        { url: `${baseUrl}/`, lastModified: new Date(), changeFrequency: "yearly", priority: 1.0 },
-        { url: `${baseUrl}/rules/starting-game`, lastModified: new Date(), changeFrequency: "yearly", priority: 0.8 },
-        { url: `${baseUrl}/rules/team-creation`, lastModified: new Date(), changeFrequency: "yearly", priority: 0.8 },
-        { url: `${baseUrl}/rules/bonus-malus`, lastModified: new Date(), changeFrequency: "yearly", priority: 0.8 },
-        { url: `${baseUrl}/rules/personalization`, lastModified: new Date(), changeFrequency: "yearly", priority: 0.8 },
-        { url: `${baseUrl}/rules/site-explanation`, lastModified: new Date(), changeFrequency: "yearly", priority: 0.8 },
-    ]
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/rules/starting-game`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: `${baseUrl}/rules/team-creation`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: `${baseUrl}/rules/bonus-malus`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: `${baseUrl}/rules/personalization`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: `${baseUrl}/rules/site-explanation`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: `${baseUrl}/auth/login`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/auth/signin`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+  ];
 }
